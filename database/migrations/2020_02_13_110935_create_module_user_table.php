6e0352a8ae71495ec6e7b27aaacfebaf1752a17c
Shift <shift@laravelshift.com>
2022-01-30 11:23:14 +0000

    Adopt anonymous migrations

--- database/migrations/2020_02_13_110935_create_module_user_table.php
+++ database/migrations/2020_02_13_110935_create_module_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateModuleUserTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateModuleUserTable extends Migration
     {
         Schema::dropIfExists('module_user');
     }
-}
+};
